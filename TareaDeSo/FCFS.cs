1456156f305d0d45437f088e4a976811e6c7604d
Isaac2797 <36213348+Isaac2797@users.noreply.github.com>
2018-03-11 16:45:52 -0600

    Add files via upload

--- TareaDeSo/FCFS.cs
+++ TareaDeSo/FCFS.cs
@@ -10,30 +10,27 @@ namespace Planificador_de_Procesos
     {
         private int[] _process;
         int[] _waitingtime;
+        int[] _responsetime;
         Random rt = new Random();
 
         public FCFS(int size)
         {
             _process = new int[size];
             _waitingtime = new int[size];
+            _responsetime = new int[size];
         }
 
-        /*public void getProcess(int max)
-        {
-            for (int i = 0; i < _process.Length; i++)
-            {
-                _process[i] = rt.Next(1, max + 1);
-            }
-        }*/
         int tat = 0;
+        int ret = 0;
         int temp = 0;
+
         public float Waitedtime(int max)
         {
            for(int i = 0; i < _process.Length; i++)
             {
                     _process[i] = rt.Next(1, max + 1);
             }
-           for(int i = 0; i < _process.Length; i++)
+           for(int i = 0; i < _process.Length-1; i++)
             {
                 _waitingtime[i] = temp + _process[i];
                 temp += _process[i];
@@ -42,7 +39,17 @@ namespace Planificador_de_Procesos
             return (float)tat / _process.Length;
         }
 
-
+        public float Responetime()
+        {
+            temp = 0;
+            for (int i = 0; i < _process.Length; i++)
+            {
+                _responsetime[i] = temp + _process[i];
+                temp += _process[i];
+                ret += _responsetime[i];
+            }
+            return (float)ret / _process.Length;
+        }
         public string show()
         {
             string processes = "";
@@ -51,6 +58,22 @@ namespace Planificador_de_Procesos
                 processes += "Proceso " + (i + 1) + " es: " + _process[i] + "\r\n";
             }
             return processes;
+        }
+
+        public void FillingShortest()
+        {
+            for (int j = 0; j < _process.Length; j++)
+            {
+                for (int i = 0; i < _process.Length; i++)
+                {
+                    if (_process[i] > _process[i + 1])
+                    {
+                        int shortest = _process[i];
+                        _process[i] = _process[i + 1];
+                        _process[i + 1] = shortest;
+                    }
+                }
+            }
         }
     }
 }
--- TareaDeSo/Form1.cs
+++ TareaDeSo/Form1.cs
@@ -8,13 +8,37 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace TareaDeSo
+namespace Planificador_de_Procesos
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        FCFS fcfs;
+
+        private void btnFCFS_Click(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt32(txtProcess.Text);
+            int max = Convert.ToInt32(txtMaxtime.Text);
+            fcfs = new FCFS(num);
+            txtTet.Text = Convert.ToString(fcfs.Waitedtime(max));
+            txtShow.Text = fcfs.show();
+            txtRet.Text = Convert.ToString(fcfs.Responetime());
+            
+        }
+
+        private void btnSjfs_Click(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt32(txtProcess.Text);
+            int max = Convert.ToInt32(txtMaxtime.Text);
+            fcfs = new FCFS(num);
+            txtShow.Text = fcfs.show();
+            txtTet.Text = Convert.ToString(fcfs.Waitedtime(max));
+            fcfs.FillingShortest();
+            txtRet.Text = Convert.ToString(fcfs.Responetime());
         }
     }
 }
